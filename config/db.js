7599c8b1e60d12703b9cc0d9b69ffd90174bc6f1
DwngSagn38 <shallot38hk@gmail.com>
2024-03-21 12:32:52 +0700

    sang

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const uri = "mongodb+srv://shallot38hk:dtsxXpGAGImzmYcS@cluster0.4vigwt9.mongodb.net/asm_ph42693"
+
+
+const connect = async () => {
+    try{
+        await mongoose.connect(uri);
+        console.log('connect success')
+    }catch(err){
+        console.log(err);
+        console.log('connect fail')
+    }
+}
+
+module.exports = {connect}
